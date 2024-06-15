07117e1fc4a5f5ad9308a815a42cee8f46502dc8
TeNko Tsukimi <45878717+cmnfriend@users.noreply.github.com>
2024-06-15 12:08:22 +0800

    添加主要实验结果
    超参：seqlora=icrlora(除λ1=λ2=0外)=olora

---
+++ logs_and_outputs/order1_seqft/hyparam.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+set -x
+
+export CUDA_DEVICE_ORDER="PCI_BUS_ID"
+export TRANSFORMERS_CACHE=/root/.cache/huggingface
+
+port=$(shuf -i25000-30000 -n1)
+ 
+# bash scripts/full_param_t5.sh> logs_and_outputs/full_param_t5/logs/train_and_infer.log 2>&1 &
+
+port=$(shuf -i25000-30000 -n1)
+CUDA_VISIBLE_DEVICES=2,3 deepspeed --master_port $port src/run_uie_lora.py \
+   --do_train \
+   --do_predict \
+   --predict_with_generate \
+   --model_name_or_path /root/MODELS/t5-large \
+   --data_dir CL_Benchmark \
+   --task_config_dir configs/order1_configs/dbpedia \
+   --instruction_file configs/instruction_config.json \
+   --instruction_strategy single \
+   --output_dir logs_and_outputs/full_param_t5/outputs/1-dbpedia \
+   --per_device_train_batch_size 8 \
+   --per_device_eval_batch_size 128 \
+   --gradient_accumulation_steps 4 \
+   --learning_rate 1e-03 \
+   --num_train_epochs 1 \
+   --deepspeed configs/ds_configs/stage2.config \
+   --run_name order1_round1 \
+   --max_source_length 512 \
+   --max_target_length 50 \
+   --generation_max_length 50 \
+   --add_task_name True \
+   --add_dataset_name True \
+   --overwrite_output_dir \
+   --overwrite_cache \
+   --lr_scheduler_type constant \
+   --warmup_steps 0 \
+   --logging_strategy steps \
+   --logging_steps 10 \
+   --evaluation_strategy no \
+   --save_strategy no \
+   --save_steps 1500 \
+   --lamda_1 0.5 \
+   --lamda_2 0
+
+sleep 5
+
+port=$(shuf -i25000-30000 -n1)
+CUDA_VISIBLE_DEVICES=2,3 deepspeed --master_port $port src/run_uie_lora.py \
+   --do_train \
+   --do_predict \
+   --predict_with_generate \
+   --model_name_or_path logs_and_outputs/full_param_t5/outputs/1-dbpedia \
+   --data_dir CL_Benchmark \
+   --task_config_dir configs/order1_configs/amazon \
+   --instruction_file configs/instruction_config.json \
+   --instruction_strategy single \
+   --output_dir logs_and_outputs/full_param_t5/outputs/2-amazon \
+   --per_device_train_batch_size 8 \
+   --per_device_eval_batch_size 128 \
+   --gradient_accumulation_steps 4 \
+   --learning_rate 1e-04 \
+   --num_train_epochs 1 \
+   --deepspeed configs/ds_configs/stage2.config \
+   --run_name order1_round2 \
+   --max_source_length 512 \
+   --max_target_length 50 \
+   --generation_max_length 50 \
+   --add_task_name True \
+   --add_dataset_name True \
+   --overwrite_output_dir \
+   --overwrite_cache \
+   --lr_scheduler_type constant \
+   --warmup_steps 0 \
+   --logging_strategy steps \
+   --logging_steps 10 \
+   --evaluation_strategy no \
+   --save_strategy no \
+   --save_steps 1500 \
+   --lamda_1 0.5 \
+   --lamda_2 0
+
+sleep 5
+
+port=$(shuf -i25000-30000 -n1)
+CUDA_VISIBLE_DEVICES=2,3 deepspeed --master_port $port src/run_uie_lora.py \
+   --do_train \
+   --do_predict \
+   --predict_with_generate \
+   --model_name_or_path logs_and_outputs/full_param_t5/outputs/2-amazon \
+   --data_dir CL_Benchmark \
+   --task_config_dir configs/order1_configs/yahoo \
+   --instruction_file configs/instruction_config.json \
+   --instruction_strategy single \
+   --output_dir logs_and_outputs/full_param_t5/outputs/3-yahoo \
+   --per_device_train_batch_size 8 \
+   --per_device_eval_batch_size 128 \
+   --gradient_accumulation_steps 4 \
+   --learning_rate 1e-04 \
+   --num_train_epochs 1 \
+   --deepspeed configs/ds_configs/stage2.config \
+   --run_name order1_round3 \
+   --max_source_length 512 \
+   --max_target_length 50 \
+   --generation_max_length 50 \
+   --add_task_name True \
+   --add_dataset_name True \
+   --overwrite_output_dir \
+   --overwrite_cache \
+   --lr_scheduler_type constant \
+   --warmup_steps 0 \
+   --logging_strategy steps \
+   --logging_steps 10 \
+   --evaluation_strategy no \
+   --save_strategy no \
+   --save_steps 1500 \
+   --lamda_1 0.5 \
+   --lamda_2 0
+
+sleep 5
+
+port=$(shuf -i25000-30000 -n1)
+CUDA_VISIBLE_DEVICES=2,3 deepspeed --master_port $port src/run_uie_lora.py \
+   --do_train \
+   --do_predict \
+   --predict_with_generate \
+   --model_name_or_path logs_and_outputs/full_param_t5/outputs/3-yahoo \
+   --data_dir CL_Benchmark \
+   --task_config_dir configs/order1_configs/agnews \
+   --instruction_file configs/instruction_config.json \
+   --instruction_strategy single \
+   --output_dir logs_and_outputs/full_param_t5/outputs/4-agnews \
+   --per_device_train_batch_size 8 \
+   --per_device_eval_batch_size 128 \
+   --gradient_accumulation_steps 4 \
+   --learning_rate 1e-04 \
+   --num_train_epochs 1 \
+   --deepspeed configs/ds_configs/stage2.config \
+   --run_name order1_round4 \
+   --max_source_length 512 \
+   --max_target_length 50 \
+   --generation_max_length 50 \
+   --add_task_name True \
+   --add_dataset_name True \
+   --overwrite_output_dir \
+   --overwrite_cache \
+   --lr_scheduler_type constant \
+   --warmup_steps 0 \
+   --logging_strategy steps \
+   --logging_steps 10 \
+   --evaluation_strategy no \
+   --save_strategy no \
+   --save_steps 1500 \
+   --lamda_1 0.5 \
+   --lamda_2 0 
